7fc6758f66a744dcc7c469c8a73918f6ca4f460a
claudia-kinsella <claudia.kinsella@ucdconnect.ie>
2017-04-20 15:25:57 +0100

    Updated app.py
    Improved mySQL queries to retrieve only the most up to date information for each individual station.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import simplejson as json
 #import pymysql
 import config
 import pandas as pd
+from jinja2 import Template
 
 app = Flask(__name__)
 
@@ -35,9 +36,16 @@ def get_db():
 
 @app.route("/dbinfo")
 def get_dbinfo():
-    # this function shows the tables in your database
+    # this function shows the important station information in your database based on the latest updates of each station
     sql = """
-    SELECT number, address, banking, status, available_bike_stands, available_bikes, Weather, Temp, Windspeed FROM dbProject
+    SELECT db.address, number, banking, status, available_bike_stands, available_bikes, Weather, Temp, last_update
+    FROM dbProject db
+    INNER JOIN
+        (SELECT address, MAX(dbID) AS LastUpdate
+        FROM dbProject
+        GROUP BY address) groupeddb 
+    ON db.address = groupeddb.address 
+    AND db.dbID = groupeddb.LastUpdate
     ;
     """.format(config.DB)
     engine = get_db()
@@ -46,7 +54,6 @@ def get_dbinfo():
     print(res)
     return jsonify(data=res), render_template("index.html"
                                              )
-
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -68,13 +75,14 @@ def get_stations():
 
 @app.route('/station/<int:number>')
 def station(number):
-    # show the station with the given id, the id is an integer
-
-    # this line would just return a simple string echoing the station_id
-    # return 'Retrieving info for Station: {}'.format(station_id)
-
-    # select the station info from the db
-    sql = """SELECT * FROM dbProject WHERE number = {}""".format(number)
+    sql = """SELECT db.address, db.number, db.banking, db.status, db.available_bike_stands, db.available_bikes, db.Weather, db.Temp, db.last_update
+    FROM dbProject db
+    INNER JOIN
+        (SELECT address, MAX(dbID) AS LastUpdate
+        FROM dbProject
+        GROUP BY address) groupeddb 
+    ON db.address = groupeddb.address 
+    AND db.dbID = groupeddb.LastUpdate""".format(number)
     engine = get_db() 
     rows = engine.execute(sql).fetchall()  # we use fetchall(), but probably there is only one station
     res = [dict(row.items()) for row in rows]  # use this formula to turn the rows into a list of dicts
@@ -99,20 +107,6 @@ def get_occupied(number):
     for row in rows:
         data.append(dict(row))
     return jsonify(available=data)
-    
-#@app.route('/occupancy/<int:number>')
-#def get_occupied_bikes(number):
-#    engine = get_db()
-#    df = pd.read_sql_query('SELECT available_bike_stands FROM dbProject WHERE number = {};', engine, params={'number':number})
-##    df['last_update_date'] = pd.to_datetime(df.last_update_date, unit='ms')
-##    df.set_index('last_update_date', inplace=True)
-#    res = df['available_bike_stands'].resample('1d').mean()
-#    print(res)
-#    return jsonify(data=json.dumps(list(zip(map(lambda x:x.isoformat(), res.index), res.values))))
-
-#@app.route('/twitter')
-#def twitter():
-    
 
 if __name__ == "__main__":
     app.run(debug=True)
