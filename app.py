fee0dcd652b7aa967357ba8ef85bddde27d99b75
cwalsh94 <walshc779@gmail.com>
2017-04-21 16:04:36 +0100

    updated app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, g, jsonify
 from sqlalchemy import create_engine
-import simplejson as json
+# import simplejson as json
 #import mysql.connector
-#import MySQLdb
+# import MySQLdb
 #import pymysql
 import config
 import pandas as pd
-from jinja2 import Template
 
 app = Flask(__name__)
 
@@ -18,7 +17,8 @@ def main():
 
     
 def connect_to_database():
-    db_str = "mysql+mysqldb://{}:{}@{}:{}/{}"
+    # db_str = "mysql+mysqldb://{}:{}@{}:{}/{}"
+    db_str = "mysql+mysqlconnector://{}:{}@{}:{}/{}"
     engine = create_engine(db_str.format(config.USER,
                                         config.PASSWORD,
                                         config.URI,
@@ -36,9 +36,9 @@ def get_db():
 
 @app.route("/dbinfo")
 def get_dbinfo():
-    # this function shows the important station information in your database based on the latest updates of each station
+    # this function shows the tables in your database
     sql = """
-    SELECT db.address, number, banking, status, available_bike_stands, available_bikes, Weather, Temp, last_update
+    SELECT db.lat, db.lon, db.address, db.last_update
     FROM dbProject db
     INNER JOIN
         (SELECT address, MAX(dbID) AS LastUpdate
@@ -51,9 +51,11 @@ def get_dbinfo():
     engine = get_db()
     rows = engine.execute(sql).fetchall()
     res = [dict(row.items()) for row in rows]
-    print(res)
-    return jsonify(data=res), render_template("index.html"
-                                             )
+    # otto_data_JSON = jsonify(res)
+    # return render_template("testfile.html", otto_data_JSON=otto_data_JSON)
+    # return otto_data_JSON, render_template("testfile.html")
+    # test = jsonify(res)
+    return jsonify(res)
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -75,14 +77,13 @@ def get_stations():
 
 @app.route('/station/<int:number>')
 def station(number):
-    sql = """SELECT db.address, db.number, db.banking, db.status, db.available_bike_stands, db.available_bikes, db.Weather, db.Temp, db.last_update
-    FROM dbProject db
-    INNER JOIN
-        (SELECT address, MAX(dbID) AS LastUpdate
-        FROM dbProject
-        GROUP BY address) groupeddb 
-    ON db.address = groupeddb.address 
-    AND db.dbID = groupeddb.LastUpdate""".format(number)
+    # show the station with the given id, the id is an integer
+
+    # this line would just return a simple string echoing the station_id
+    # return 'Retrieving info for Station: {}'.format(station_id)
+
+    # select the station info from the db
+    sql = """SELECT * FROM dbProject WHERE number = {}""".format(number)
     engine = get_db() 
     rows = engine.execute(sql).fetchall()  # we use fetchall(), but probably there is only one station
     res = [dict(row.items()) for row in rows]  # use this formula to turn the rows into a list of dicts
@@ -107,6 +108,20 @@ def get_occupied(number):
     for row in rows:
         data.append(dict(row))
     return jsonify(available=data)
+    
+#@app.route('/occupancy/<int:number>')
+#def get_occupied_bikes(number):
+#    engine = get_db()
+#    df = pd.read_sql_query('SELECT available_bike_stands FROM dbProject WHERE number = {};', engine, params={'number':number})
+##    df['last_update_date'] = pd.to_datetime(df.last_update_date, unit='ms')
+##    df.set_index('last_update_date', inplace=True)
+#    res = df['available_bike_stands'].resample('1d').mean()
+#    print(res)
+#    return jsonify(data=json.dumps(list(zip(map(lambda x:x.isoformat(), res.index), res.values))))
+
+#@app.route('/twitter')
+#def twitter():
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
